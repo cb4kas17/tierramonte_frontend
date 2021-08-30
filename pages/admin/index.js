88bad0d3af96d5b2f34e5b1948a97c4fb41a2c6f
unknown <christianbryneportugal@yahoo.com>
2021-08-31 04:10:06 +0800

    Create user

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,15 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import AdminPageContainer from '../../components/AdminPageComponent/AdminPageContainer';
 function AdminPage(props) {
-      const [allUsers, setAllUsers] = useState();
-      const DUMMY_DATA = [
-            { name: 'ABCD', role: 0 },
-            { name: 'HEKTOR', role: 1 },
-            { name: 'ALDAB', role: 2 },
-            { name: 'XD', role: 3 },
-            { name: 'JOBERT', role: 4 },
-      ];
+      const [allUsers, setAllUsers] = useState([]);
+      const [profile, setProfile] = useState([]);
+    
       const getAllUsers = async () => {
             try {
                   const response = await axios.get(
@@ -17,15 +12,31 @@ function AdminPage(props) {
                         { withCredentials: true }
                   );
                   setAllUsers(response.data.users);
-                  console.log(response.data.users);
+                  
+            } catch (error) {
+                  console.log(error);
+            }
+      };
+       
+      const getProfile = async () => {
+            try {
+                  const response = await axios.get(
+                        'http://localhost:4000/api/admin',
+                        { withCredentials: true }
+                  );
+                  setProfile(response.data.user);
+                 
             } catch (error) {
                   console.log(error);
             }
       };
 
+    
+
       useEffect(() => {
             getAllUsers();
-      }, []);
+            getProfile();
+      },[]);
       return (
             <div>
                   <AdminPageContainer data={allUsers} />

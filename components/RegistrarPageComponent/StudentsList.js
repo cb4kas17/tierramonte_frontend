b757cb71a46ed6873936f1e015908e08c691b081
Christian Bryan Portugal <christianbryneportugal@yahoo.com>
2021-09-06 19:25:54 +0800

    filter + dropdown done

--- components/RegistrarPageComponent/StudentsList.js
+++ components/RegistrarPageComponent/StudentsList.js
@@ -5,21 +5,49 @@ import StudentsListItem from './StudentsListItem';
 
 function StudentsList(props) {
       const [search, setSearch] = useState('');
+      const [yearLevel, setYearLevel] = useState('all');
 
       const searchBarHandler = (event) => {
             setSearch(event.target.value);
       };
 
       const filter = (list) => {
-            return list.filter(
-                  (data) => data.lastName.toLowerCase().indexOf(search) > -1
-            );
+            if (yearLevel === 'all') {
+                  return list.filter(
+                        (data) =>
+                              data.lastName.toLowerCase().indexOf(search) > -1
+                  );
+            } else {
+                  return list.filter(
+                        (data) =>
+                              data.yearLevel == yearLevel &&
+                              data.lastName.toLowerCase().indexOf(search) > -1
+                  );
+            }
       };
       console.log(search);
+      const yearLevelHandler = (event) => {
+            setYearLevel(event.target.value);
+      };
+      console.log(yearLevel);
       return (
             <div className={styles.container}>
-                  <div className={styles.headerContainer}>
-                        <h1 className={styles.header}>Students</h1>
+                  <h1 className={styles.header}>Students</h1>
+                  <div className={styles.filterContainer}>
+                        <select
+                              name="yearlevel"
+                              id="yearlevel"
+                              onChange={yearLevelHandler}
+                              className={styles.select}
+                        >
+                              <option value="all">All</option>
+                              <option value="7">7</option>
+                              <option value="8">8</option>
+                              <option value="9">9</option>
+                              <option value="10">10</option>
+                              <option value="11">11</option>
+                              <option value="12">12</option>
+                        </select>
                         <input
                               className={styles.input}
                               type="search"
@@ -30,6 +58,7 @@ function StudentsList(props) {
                               value={props.searchBind}
                         />
                   </div>
+
                   <div className={styles.columnName}>Students</div>
                   <ul className={styles.listContainer}>
                         <h4 className={styles.name}>Name</h4>
